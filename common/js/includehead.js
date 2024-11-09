f11ddca7e3731f42375c8d72f236e1959552b16f
toshi <t.matsu.todo@gmail.com>
2024-11-09 15:28:28 +0900

    インクルードをheadだけ、headerだけに変更、なんか上手くいってない

--- common/js/includehead.js
+++ common/js/includehead.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", async function () {
   try {
     const response = await fetch("/common/template/head-w.html");
     const headContent = await response.text();
-    document.documentElement.innerHTML =
-      headContent + headerContent + document.documentElement.innerHTML;
-  } catch (error) {}
+    document.head.innerHTML = headContent;
+  } catch (error) {
+    console.error("Error fetching head:", error);
+  }
 });
--- common/js/includeheader.js
+++ common/js/includeheader.js
@@ -1,5 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const response = fetch("/common/template/header-w.html");
-  const headerContent = response.text();
-  document.getElementById("header-placeholder").innerHTML = headerContent;
+document.addEventListener("DOMContentLoaded", async function () {
+  try {
+    const response = await fetch("/common/template/header-w.html");
+    const headerContent = await response.text();
+    document.getElementById("header-placeholder").innerHTML = headerContent;
+  } catch (error) {
+    console.error("Error fetching header:", error);
+  }
 });
